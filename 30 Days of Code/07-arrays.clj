4a4c378c354ca01009ff7576b9403f42f0eacb08
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2016-12-06 19:36:12 +0300

    hotfix

--- 30 Days of Code/07-arrays.clj
+++ 30 Days of Code/07-arrays.clj
@@ -4,18 +4,16 @@
 (defn my-reverse [lst] (reduce conj '() lst))
 
 (
-        let [
-          n_t (read-line)
-          n (Integer/parseInt n_t)
-        ]
-
-      (
-        let [
-          arr_temp (read-line)
-          arr_t (split arr_temp #"\s+")
-          arr (map #(Integer/parseInt %) arr_t)
-        ]
-        (apply println (my-reverse arr))
-      )
-
+  let [
+    n_t (read-line)
+    n (Integer/parseInt n_t)
+  ]
+    (
+      let [
+        arr_temp (read-line)
+        arr_t (split arr_temp #"\s+")
+        arr (map #(Integer/parseInt %) arr_t)
+      ]
+      (apply print (my-reverse arr))
+    )
 )
