bdc222ed33f5377cb8f99a0ba3ba58e94e389ec2
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2016-12-09 14:09:03 +0300

    some more solutions

---
+++ Functional Programming/24-lambda-march-compute-the-area-of-a-polygon.clj
@@ -0,0 +1,27 @@
+; https://www.hackerrank.com/challenges/lambda-march-compute-the-area-of-a-polygon
+
+(defn read-coordinates []
+  (map #(Float/parseFloat %) (clojure.string/split (read-line) #" ")))
+
+(def coordinates [[1043 770][551 990][681 463]])
+
+(defn calc [[x_i y_i] [x_j y_j]]
+  (- (* x_i y_j) (* x_j y_i)))
+
+(defn shift-head [coordinates]
+  (let [[head tail] (split-at 1 coordinates)]
+    (concat tail head)))
+
+(defn area [coordinates]
+  (->> (map calc coordinates (shift-head coordinates))
+    (apply +)
+    (* 0.5)
+    (Math/abs)))
+
+(
+  let [
+    n (Integer/parseInt (read-line))
+    coordinates (doall (repeatedly n read-coordinates))
+  ]
+  (println (area coordinates))
+)
