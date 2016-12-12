2295d8a64488e15f33f534d71e8713d4059237c2
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2016-12-12 12:39:19 +0300

    some more solutions

---
+++ 30 Days of Code/13-abstract-classes.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/30-abstract-classes
+
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        super().__init__(title, author)
+        self.price = price
+
+    def display(self):
+        print("Title:", self.title)
+        print("Author:", self.author)
+        print("Price:", self.price)
