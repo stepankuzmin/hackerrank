50a6003b969f4a515fa86bca9a2bc22fb7051a4f
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2016-12-13 13:20:22 +0300

    some more solutions

---
+++ 30 Days of Code/14-scope.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/30-scope
+
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+
+    def computeDifference(self):
+        self.maximumDifference = max(self.__elements) - min(self.__elements)
