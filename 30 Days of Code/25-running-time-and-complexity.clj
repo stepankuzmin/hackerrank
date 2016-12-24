afc50cc8d79ef7c3c90e3aae015e06ad51e55aae
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2016-12-24 16:54:24 +0300

    fix

--- 30 Days of Code/25-running-time-and-complexity.clj
+++ 30 Days of Code/25-running-time-and-complexity.clj
@@ -1,36 +1,18 @@
 ; https://www.hackerrank.com/challenges/30-running-time-and-complexity
 
-; (def rem-not-0
-;   (memoize (fn [a b]
-;     (not= 0 (rem a b)))))
-
-; (defn primes-in-range [collection]
-;   (loop
-;     [
-;       head (first collection)
-;       tail (rest collection)
-;       primes []
-;     ]
-;     (if (empty? tail)
-;       (conj primes head)
-;       (recur
-;         (first tail)
-;         (rest (doall (filter #(rem-not-0 % head) tail)))
-;         (conj primes head)))))
-
-(defn primes-in-range [max]
-  (let [enqueue (fn [sieve n factor]
-                  (let [m (+ n (+ factor factor))]
-                    (if (sieve m)
-                      (recur sieve m factor)
-                      (assoc sieve m factor))))
-        next-sieve (fn [sieve candidate]
-                     (if-let [factor (sieve candidate)]
-                       (-> sieve
-                         (dissoc candidate)
-                         (enqueue candidate factor))
-                       (enqueue sieve candidate candidate)))]
-    (cons 2 (vals (reduce next-sieve {} (range 3 max 2))))))
+(defn primes-in-range [collection]
+  (loop
+    [
+      head (first collection)
+      tail (rest collection)
+      primes []
+    ]
+    (if (empty? tail)
+      (conj primes head)
+      (recur
+        (first tail)
+        (rest (doall (filter #(not= 0 (rem % head)) tail)))
+        (conj primes head)))))
 
 (defn not-divisible-by [collection n]
   (loop
@@ -43,11 +25,11 @@
       (empty? tail) true
       :else (recur (first tail) (rest tail)))))
 
-(defn read-integer []
-  (Integer/parseInt (read-line)))
-
 (defn is-prime [primes n]
   (or (.contains primes n) (not-divisible-by primes n)))
+
+(defn read-integer []
+  (Integer/parseInt (read-line)))
 
 (let
   [
@@ -55,13 +37,11 @@
     integers (doall (repeatedly N read-integer))
     largest (apply max integers)
     limit (Math/ceil (Math/sqrt largest))
-    primes (primes-in-range limit)
+    primes (cons 2 (primes-in-range (range 3 limit 2)))
   ]
   (doall
     (map
-      #(if (and (is-prime primes %) (>= % 2))
+      #(if (and (>= % 2) (is-prime primes %))
         (println "Prime")
         (println "Not prime"))
-      integers)
-  )
-)
+      integers)))
