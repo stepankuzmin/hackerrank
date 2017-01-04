496d7d4c22a79701fb44df6afff1368ad57e24e1
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-01-04 18:48:10 +0300

    some more solutions

---
+++ Cracking the Coding Interview/01-array-left-rotation.js
@@ -0,0 +1,38 @@
+// https://www.hackerrank.com/challenges/ctci-array-left-rotation
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function lr(arr, n, k) {
+  var d = k % n;
+  return arr.slice(d, n).concat(arr.slice(0, d));
+}
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    a = readLine().split(' ');
+    a = a.map(Number);
+    const result = lr(a, n, k);
+    console.log(result.join(' '))
+}
