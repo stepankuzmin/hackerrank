e03eb81e94fc3724380aad757e0341caae470c74
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-04-23 15:10:41 +0300

    fix queue-using-two-stacks

--- Cracking the Coding Interview/06-queue-using-two-stacks.js
+++ Cracking the Coding Interview/06-queue-using-two-stacks.js
@@ -1,13 +1,46 @@
 // https://www.hackerrank.com/challenges/ctci-queue-using-two-stacks
 
-const queue = [];
+class Queue {
+  constructor() {
+    this.inbox = [];
+    this.outbox = [];
+  }
+
+  fillOutbox() {
+    if (this.outbox.length === 0) {
+      while (this.inbox.length !== 0) {
+        this.outbox.push(this.inbox.pop())
+      }
+    }
+  }
+
+  enqueue(value) {
+    this.inbox.push(value);
+  }
+
+  dequeue() {
+    this.fillOutbox();
+    return this.outbox.pop();
+  }
+
+  peek() {
+    this.fillOutbox();
+    return this.outbox[this.outbox.length - 1];
+  }
+}
+
+const queue = new Queue();
+
+const ENQUEUE = 1;
+const DEQUEUE = 2;
+const PRINT = 3;
 
 function processData(input) {
   const [command, argument] = input.split(' ');
-  switch (command) {
-    case '1': queue.push(argument); break;
-    case '2': queue.shift(); break;
-    case '3': console.log(queue[0]); break;
+  switch (parseInt(command, 10)) {
+    case ENQUEUE: queue.enqueue(argument); break;
+    case DEQUEUE: queue.dequeue(); break;
+    case PRINT: console.log(queue.peek()); break;
     default: throw new Error(`Unknown command ${command}`);
   }
 }
