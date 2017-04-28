139454b453431547ba052a832553e17917f7df2e
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-04-28 15:05:58 +0300

    WIP find the running median

--- Cracking the Coding Interview/08-find-the-running-median.js
+++ Cracking the Coding Interview/08-find-the-running-median.js
@@ -22,6 +22,72 @@ function readLine() {
 
 /////////////// ignore above this line ////////////////////
 
+class Heap {
+  constructor(comp) {
+    this.array = [];
+    this.comp = (a, b) => a > b;
+  }
+
+  size() {
+    return this.array.length;
+  }
+
+  push(value) {
+    this.array.push(value);
+    this.bubbleUp(this.array.length - 1);
+  }
+
+  pop() {
+    if (this.array.length === 0) {
+      return;
+    }
+
+    const value = this.array[0];
+    const lastIndex = this.array.length - 1;
+    this.array[0] = this.array[lastIndex];
+    this.array.length = lastIndex;
+    if (lastIndex > 0) {
+      this.bubbleDown(0);
+    }
+
+    return value;
+  }
+
+  swap(a, b) {
+    const temp = this.array[a];
+    this.array[a] = this.array[b];
+    this.array[b] = temp;
+  }
+
+  bubbleUp(pos) {
+    if (pos <= 0) {
+      return;
+    }
+
+    const parent = Math.floor((pos - 1) / 2);
+    if (this.comp(this.array[pos], this.array[parent])) {
+      this.swap(pos, parent);
+      this.bubbleUp(parent);
+    }
+  }
+
+  bubbleDown(pos) {
+    const left = 2 * pos + 1;
+    const right = left + 1;
+    let largest = pos;
+    if (left < this.array.length && this.comp(this.array[left], this.array[largest])) {
+      largest = left;
+    }
+    if (right < this.array.length && this.comp(this.array[right], this.array[largest])) {
+      largest = right;
+    }
+    if (largest != pos) {
+      this.swap(largest, pos);
+      this.bubbleDown(largest);
+    }
+  }
+}
+
 median = (list) => {
   const length = list.length;
   if (length === 1) {
